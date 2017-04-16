002aff27aae71db35b3b896870ee8defff070011
Abhishek Upadhyay <Abhishek@Abhisheks-MacBook-Pro.local>
2017-04-16 15:18:30 -0700

    First Commit of our team hackathon project - Gaspump Greenfoot

---
+++ gaspump/MembershipCard.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MembershipCard here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MembershipCard extends Actor
+{
+    /**
+     * Act - do whatever the MembershipCard wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
