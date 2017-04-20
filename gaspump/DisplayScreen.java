7272208c47018445426f9c9c8a1dbc4783050d3c
Abhishek Upadhyay <abhishek.up25@gmail.com>
2017-04-20 13:32:18 -0700

    updated screen

--- gaspump/DisplayScreen.java
+++ gaspump/DisplayScreen.java
@@ -13,63 +13,71 @@ public class DisplayScreen extends Actor
      * Act - do whatever the DisplayScreen wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private String state = "";
+    private GreenfootImage currentScreenImage= null;
+    private Message message = null;
+    
     public void act() 
     {
         // Add your action code here.
         World w=getWorld();
+        currentScreenImage = getImage();
+        
+        //setMessageString("CardValidated");
+        //setMessageString("CardNotInserted");
         
     } 
     
     public void setMessageString(String msg)
     {
-        //Message message = new Message("Welcome to the gas pump station");
-        //message = new Message(msg);
-        World w=getWorld();
-        //message.showMessage(w, message);
-        //w.addObject(message,721, 154);
-        //message.setLocation(721, 154);
         
-        String imageFile = "";
+        World w=getWorld();
+        System.out.println(msg);
         
         state = msg;
         
-        greenfootImage = new GreenfootImage("/Users/Abhishek/CMPE202---Team-Hackathon-Project/gaspump/images/zipValidated.png");
         
         
-        //GreenfootImage greenfootImage1 = new GreenfootImage(greenfootImage);
+        //currentScreenImage = new GreenfootImage(msg,250, Color.WHITE, Color.BLACK);
+        //setImage(new GreenfootImage(msg,40, Color.BLACK, Color.WHITE));
+        //w.addObject(_object_, _x_, _y_)
+    
+    
         
-        //message.clear();
-        
-        message.setImage(greenfootImage1);
-        ///getWorld().addObject(message,700,160);
-        
-        
-        
-        
-        //setImage(greenfootImage);
+       
         
         if(state.equals("Card Validated"))
         {
-            message = new Message("Please enter zip code");
+            setImage(new GreenfootImage(msg,40, Color.BLACK, Color.WHITE));
+           
+        }
+        else if(state.equals("Fake"))
+        {
+            System.out.println("inside" +msg);
         
+            getImage().clear();
+            setImage(new GreenfootImage(msg,30, Color.BLACK, Color.WHITE));
         }
-        if(state.equals("CardNotInserted"))
+        
+        else 
         {
-            message = new Message("Please Scan your Card Again");
-            getWorld().addObject(message,700,160);
+            getImage().clear();
+            setImage(new GreenfootImage(msg,30, Color.BLACK, Color.WHITE));
+
         }
-        if(state.equals("ZipValidated"))
+        
+        if(state.equals("Zip Validated"))
         {
-            message = new Message("Proceed with your selection");
-            
-           // message.setImage(greenfootImage);
-            
-            getWorld().addObject(message,700,160);
+            getImage().clear();
+            setImage(new GreenfootImage(msg,40, Color.BLACK, Color.WHITE));
+           
         }
-        if(state.equals("Fake card"))
+       else
         {
-            message = new Message("Fake Card.. Can't Process);
-            getWorld().addObject(message,700,160);
+            getImage().clear();
+            setImage(new GreenfootImage(msg,30, Color.BLACK, Color.WHITE));
+
         }
+        
     }
 }
